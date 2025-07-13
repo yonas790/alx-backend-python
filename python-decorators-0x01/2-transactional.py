9ac6181f1a017d9c330fecd0df8a1b7c4265eb5a
yonas790 <yoni2752@gmail.com>
2025-07-13 16:27:47 +0300

    Transaction Management Decorator

---
+++ python-decorators-0x01/2-transactional.py
@@ -0,0 +1,33 @@
+import sqlite3
+import functools
+
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')
+        try:
+            return func(conn, *args, **kwargs)
+        finally:
+            conn.close()
+    return wrapper
+
+def transactional(func):
+    @functools.wraps(func)
+    def wrapper(conn, *args, **kwargs):
+        try:
+            result = func(conn, *args, **kwargs)
+            conn.commit()  # Commit if no error
+            return result
+        except Exception:
+            conn.rollback()  # Rollback on error
+            raise  # Re-raise the exception so it can be handled outside
+    return wrapper
+
+@with_db_connection
+@transactional
+def update_user_email(conn, user_id, new_email):
+    cursor = conn.cursor()
+    cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id))
+
+# Call the function with transaction handling
+update_user_email(user_id=1, new_email='yonas@gmail.com')
--- python-decorators-0x01/README.md
+++ python-decorators-0x01/README.md
@@ -1,2 +1,3 @@
 ## 0. Logging database Queries
 ## 1. Handle Database Connections with a Decorator
+## 2. Transaction Management Decorator
