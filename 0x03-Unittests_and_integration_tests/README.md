a24d51b0b76f775af6e9f23087b5068e77287a4d
yonas790 <yoni2752@gmail.com>
2025-07-16 17:32:25 +0300

    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/README.md
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -1,2 +1,3 @@
 ## 0. Parameterize a unit test
 ## 2. Mock HTTP calls
+## 3. Parameterize and patch
--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,7 @@ import unittest
 from parameterized import parameterized
 from unittest.mock import patch, Mock
 from utils import access_nested_map, get_json
-
+from utils import memoize
 
 class TestAccessNestedMap(unittest.TestCase):
     """Test cases for access_nested_map"""
@@ -36,4 +36,30 @@ class TestGetJson(unittest.TestCase):
 
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
+
+class TestMemoize(unittest.TestCase):
+    def test_memoize(self):
+        """Test memoize decorator caches method call"""
+
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        test_obj = TestClass()
+
+        with patch.object(test_obj, 'a_method', wraps=test_obj.a_method) as mocked_method:
+            # Call a_property twice
+            first_call = test_obj.a_property
+            second_call = test_obj.a_property
+
+            # The result must be correct
+            self.assertEqual(first_call, 42)
+            self.assertEqual(second_call, 42)
+
+            # a_method called only once
+            mocked_method.assert_called_once()
 
