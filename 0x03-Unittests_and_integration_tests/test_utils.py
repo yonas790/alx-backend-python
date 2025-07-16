a216bb77aea9efb82e20e675be236a0e293d2ffe
yonas790 <yoni2752@gmail.com>
2025-07-16 17:36:55 +0300

    fix indentation

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,6 +7,7 @@ from unittest.mock import patch, Mock
 from utils import access_nested_map, get_json
 from utils import memoize
 
+
 class TestAccessNestedMap(unittest.TestCase):
     """Test cases for access_nested_map"""
 
@@ -20,6 +21,7 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
 
         self.assertEqual(str(cm.exception), repr(path[len(nested_map):][0]))
+
 
 class TestGetJson(unittest.TestCase):
     @parameterized.expand([
@@ -37,10 +39,10 @@ class TestGetJson(unittest.TestCase):
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
 
+
 class TestMemoize(unittest.TestCase):
     def test_memoize(self):
         """Test memoize decorator caches method call"""
-
         class TestClass:
             def a_method(self):
                 return 42
@@ -55,11 +57,8 @@ class TestMemoize(unittest.TestCase):
             # Call a_property twice
             first_call = test_obj.a_property
             second_call = test_obj.a_property
-
             # The result must be correct
             self.assertEqual(first_call, 42)
             self.assertEqual(second_call, 42)
-
             # a_method called only once
             mocked_method.assert_called_once()
-
