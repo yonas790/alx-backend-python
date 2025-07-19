b5f3ba6709b07d245a7cc3ccd412a93145084a10
yonas790 <yoni2752@gmail.com>
2025-07-19 23:43:10 +0300

    explicitly define password field

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128) 
     role = models.CharField(
         max_length=10,
         choices=[('guest', 'Guest'), ('host', 'Host'), ('admin', 'Admin')],
