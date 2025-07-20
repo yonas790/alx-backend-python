7b9ddcb690156499c1de41f6a7ad4ea6624d6558
yonas790 <yoni2752@gmail.com>
2025-07-20 10:42:08 +0300

    run the application an fix errors

--- messaging_app/README.md
+++ messaging_app/README.md
@@ -2,3 +2,5 @@
 ## 1. Define data Models
 ## 2. Create serializers to define the many to many relationships
 ## 3. Build api endpoints with views
+## 4. Set up url routing
+## 5. Run and test
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +24,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_)02!)os7shk3v(s+$t(4nq-c6+74)b+_4khfwaxusser(^4-w'
+SECRET_KEY=os.getenv('SECRET_KEY')
+
+if not SECRET_KEY:
+    raise Exception("SECRET_KEY environment variable not set")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'chats.User'
 
 # Application definition
 
