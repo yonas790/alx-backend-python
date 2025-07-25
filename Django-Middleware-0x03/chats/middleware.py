17645fb7bd8621ef3d8ca177787c700a1e8b373c
yonas790 <yoni2752@gmail.com>
2025-07-25 23:29:47 +0300

    Restrict Chat Access by time

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,5 +1,7 @@
 import datetime
 
+from django.http import HttpResponseForbidden
+
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
@@ -10,5 +12,20 @@ class RequestLoggingMiddleware:
 
         with open("requests.log", "a") as log_file:
             log_file.write(log_entry)
+
+        return self.get_response(request)
+
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Current hour (0-23)
+        current_hour = datetime.datetime.now().hour
+
+        # Allow only between 6 (06:00) and 21 (21:00)
+        if current_hour < 6 or current_hour >= 21:
+            return HttpResponseForbidden("Chat access is restricted during this time.")
 
         return self.get_response(request)
--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -87,6 +87,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'chats.middleware.RequestLoggingMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware',
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
